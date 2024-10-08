05391f1b30e47fa1e795c9bebaeddae1678a924a
emregokselyzb <emre.g.yzb@gmail.com>
2024-10-08 11:09:24 +0300

    SchoolLab Rest started

---
+++ src/main/java/com/cydeo/schoollab/SchoolLabApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.schoollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/schoollab/SchoolLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.schoollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
