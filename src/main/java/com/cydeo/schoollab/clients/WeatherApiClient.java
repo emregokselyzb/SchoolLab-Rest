b638a711d756bc1572a0f41f5066ae1b27002947
emregokselyzb <emre.g.yzb@gmail.com>
2024-10-08 15:49:18 +0300

    Controller class created.

---
+++ src/main/java/com/cydeo/schoollab/clients/WeatherApiClient.java
@@ -0,0 +1,4 @@
+package com.cydeo.schoollab.clients;
+
+public interface WeatherApiClient {
+}
---
+++ src/main/java/com/cydeo/schoollab/controller/SchoolController.java
@@ -0,0 +1,10 @@
+package com.cydeo.schoollab.controller;
+
+public class SchoolController {
+
+    private final TeacherService teacherService;
+    private final StudentService studentService;
+
+    private final ParentService parentService;
+    private final AddressService addressService;
+}
