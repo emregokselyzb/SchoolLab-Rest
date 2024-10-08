3338d2b9a3f37184b5929af48fd17120942152b9
emregokselyzb <emre.g.yzb@gmail.com>
2024-10-08 18:22:08 +0300

    DTO package defined.

--- src/main/java/com/cydeo/schoollab/dto/ClassDTO.java
+++ src/main/java/com/cydeo/schoollab/dto/ClassDTO.java
@@ -1,4 +1,31 @@
 package com.cydeo.schoollab.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ClassDTO {
+
+    @JsonIgnore
+    private Long id;
+    private String name;
+    private String year;
+    private CourseDTO course;
+    private TeacherDTO teacher;
+
+
+
+
+
+
 }
