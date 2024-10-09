dcc3f043e6ad2185ec2ef1850f857f0ad3cdeab9
emregokselyzb <emre.g.yzb@gmail.com>
2024-10-09 23:54:45 +0300

    StudentService created

--- src/main/java/com/cydeo/schoollab/service/StudentService.java
+++ src/main/java/com/cydeo/schoollab/service/StudentService.java
@@ -1,4 +1,12 @@
 package com.cydeo.schoollab.service;
 
+import com.cydeo.schoollab.dto.StudentDTO;
+
+import java.util.List;
+
 public interface StudentService {
+
+    List<StudentDTO> findAll();
+
+    StudentDTO findById(Long id) throws Exception;
 }
