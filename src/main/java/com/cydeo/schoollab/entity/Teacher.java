eed9254225a045cda7c4cf3f3c86a2d5ea208f3a
emregokselyzb <emre.g.yzb@gmail.com>
2024-10-09 23:26:15 +0300

    Entity created

--- src/main/java/com/cydeo/schoollab/entity/Teacher.java
+++ src/main/java/com/cydeo/schoollab/entity/Teacher.java
@@ -1,4 +1,44 @@
 package com.cydeo.schoollab.entity;
 
-public class Teacher {
+import com.cydeo.schoollab.enums.EducationLevel;
+import com.cydeo.schoollab.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Setter
+@Getter
+@Table(name="teacher")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Teacher extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    private String email;
+    private String userName;
+    private String password;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthday;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    private EducationLevel educationLevel;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+
+
 }
