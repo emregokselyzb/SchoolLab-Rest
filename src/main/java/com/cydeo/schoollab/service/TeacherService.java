73f33ba546607338397e435f3ef41e6240da78de
emregokselyzb <emre.g.yzb@gmail.com>
2024-10-09 23:57:05 +0300

    Teacherrvice created

--- src/main/java/com/cydeo/schoollab/service/TeacherService.java
+++ src/main/java/com/cydeo/schoollab/service/TeacherService.java
@@ -1,4 +1,14 @@
 package com.cydeo.schoollab.service;
 
+import com.cydeo.schoollab.dto.TeacherDTO;
+
+import java.util.List;
+
 public interface TeacherService {
+
+    List<TeacherDTO> findAll();
+
+    TeacherDTO findById(Long id) throws Exception;
+
+    TeacherDTO createTeacher (TeacherDTO teacherDTO);
 }
