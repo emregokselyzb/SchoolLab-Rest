6d960e62266aff6b45b22fff612df03bf47e00ce
emregokselyzb <emre.g.yzb@gmail.com>
2024-10-09 23:37:44 +0300

    Address service created

--- src/main/java/com/cydeo/schoollab/service/AddressService.java
+++ src/main/java/com/cydeo/schoollab/service/AddressService.java
@@ -1,6 +1,17 @@
 package com.cydeo.schoollab.service;
 
+import com.cydeo.schoollab.dto.AddressDTO;
+
+import java.util.List;
+
 public interface AddressService {
+
+    List<AddressDTO> findAll();
+
+    AddressDTO findById(Long id) throws Exception;
+
+    AddressDTO update(AddressDTO addressDTO) throws Exception;
+    AddressDTO create(AddressDTO addressDTO) throws Exception;
 
 
 }
