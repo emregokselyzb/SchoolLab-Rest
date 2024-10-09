e485acca5fcd6b9ca992732def643096f71f841a
emregokselyzb <emre.g.yzb@gmail.com>
2024-10-09 23:44:54 +0300

    ClassService created

--- src/main/java/com/cydeo/schoollab/service/ClassService.java
+++ src/main/java/com/cydeo/schoollab/service/ClassService.java
@@ -1,4 +1,12 @@
 package com.cydeo.schoollab.service;
 
+import com.cydeo.schoollab.dto.ClassDTO;
+
+import java.util.List;
+
 public interface ClassService {
+
+    List<ClassDTO> findAll();
+
+    ClassDTO findById(Long id) throws Exception;
 }
