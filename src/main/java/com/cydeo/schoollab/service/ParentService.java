2e106482f73cc919d31a32a1f977688c38ff28c7
emregokselyzb <emre.g.yzb@gmail.com>
2024-10-09 23:47:59 +0300

    ParentService created

--- src/main/java/com/cydeo/schoollab/service/ParentService.java
+++ src/main/java/com/cydeo/schoollab/service/ParentService.java
@@ -1,4 +1,12 @@
 package com.cydeo.schoollab.service;
 
+import com.cydeo.schoollab.dto.ParentDTO;
+
+import java.util.List;
+
 public interface ParentService {
+
+    List<ParentDTO> findAll();
+
+    ParentDTO findById(Long id) throws Exception;
 }
