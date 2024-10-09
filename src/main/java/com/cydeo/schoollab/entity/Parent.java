a096c3ea3079c13222fc983b5210bed2be200736
emregokselyzb <emre.g.yzb@gmail.com>
2024-10-09 23:10:24 +0300

    Entity created

--- src/main/java/com/cydeo/schoollab/entity/Parent.java
+++ src/main/java/com/cydeo/schoollab/entity/Parent.java
@@ -1,4 +1,30 @@
 package com.cydeo.schoollab.entity;
 
-public class Parent {
+import com.cydeo.schoollab.enums.Status;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+public class Parent extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+    private String profession;
+    private String phoneNumber;
+
+
+    private String email;
+    private String userName;
+    private String password;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthday;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
 }
