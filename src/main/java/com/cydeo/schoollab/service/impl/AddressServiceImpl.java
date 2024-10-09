ab572e7c35fc2555655b32458b5b7c549b16964b
emregokselyzb <emre.g.yzb@gmail.com>
2024-10-09 23:44:38 +0300

    Address serviceImpl created

--- src/main/java/com/cydeo/schoollab/service/impl/AddressServiceImpl.java
+++ src/main/java/com/cydeo/schoollab/service/impl/AddressServiceImpl.java
@@ -1,4 +1,32 @@
 package com.cydeo.schoollab.service.impl;
 
-public class AddressServiceImpl {
+import com.cydeo.schoollab.dto.AddressDTO;
+import com.cydeo.schoollab.service.AddressService;
+
+import java.util.List;
+
+public class AddressServiceImpl implements AddressService {
+
+    private String accessKey;
+    private final AddressRepository addressRepository;
+
+    @Override
+    public List<AddressDTO> findAll() {
+        return null;
+    }
+
+    @Override
+    public AddressDTO findById(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public AddressDTO update(AddressDTO addressDTO) throws Exception {
+        return null;
+    }
+
+    @Override
+    public AddressDTO create(AddressDTO addressDTO) throws Exception {
+        return null;
+    }
 }
